8bfa2a744fb448daef7442e9ae7da9eced38bf52
VICTOR ANDREE SEVILLANO VEGA <U21221311@utp.edu.pe>
2026-01-22 00:23:46 -0500

    Add dependecies

---
+++ src/main/java/com/reactiveinbox/ReactiveinboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reactiveinbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveinboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveinboxServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactiveinbox/ReactiveinboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveinbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveinboxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
